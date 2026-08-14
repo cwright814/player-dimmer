0586e9eef6afe1c34b2e620a28e1cbd9716f21a2
Christopher Wright <cwright814@gmail.com>
2026-08-14 01:44:22 -0400

    Make my personal build script work between my two developer machines

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -e
 # You will need to set up your own build workflow. An LLM can likely adapt this file for you if you are not sure where to begin.
 # -Christopher
 
-# Path to GitHub forks
-FORKS="/home/cwright/Projects/fun/minecraft"
+# Path to Moonrise (contains Minecraft source in Fabric's loom cache)
+MOONRISE="../Moonrise"
 
-# Dependency versions
+# Gradle Cache Dependencies (build Moonrise to generate)
+GRADLE_CACHE="$HOME/.gradle/caches/modules-2/files-2.1"
 FABRIC_LOADER="0.14.10/638450f4cfa4b1b25d6b05958284e5f86b638f38/fabric-loader-0.14.10"
 SPONGE_MIXIN="0.17.0+mixin.0.8.7/cf31463202f72d03b0ef1e1e38e8ee71b7faaab6/sponge-mixin-0.17.0+mixin.0.8.7"
 GSON="2.13.2/48b8230771e573b54ce6e867a9001e75977fe78e/gson-2.13.2"
@@ -25,17 +26,17 @@ rm -rf build/classes
 mkdir -p build/classes
 
 # Collect classpath
-CP="$FORKS/Moonrise/.gradle/loom-cache/minecraftMaven/net/minecraft/minecraft-merged-2ffda7bfea/26.1.2/minecraft-merged-2ffda7bfea-26.1.2.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/net.fabricmc/fabric-loader/$FABRIC_LOADER.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/net.fabricmc/sponge-mixin/$SPONGE_MIXIN.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/com.google.code.gson/gson/$GSON.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/org.jspecify/jspecify/$JSPECIFY.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/com.mojang/brigadier/$BRIGADIER.jar"
-CP="$CP:/home/cwright/.gradle/caches/modules-2/files-2.1/it.unimi.dsi/fastutil/$FASTUTIL.jar"
+CP="$MOONRISE/.gradle/loom-cache/minecraftMaven/net/minecraft/minecraft-merged-2ffda7bfea/26.1.2/minecraft-merged-2ffda7bfea-26.1.2.jar"
+CP="$CP:$GRADLE_CACHE/net.fabricmc/fabric-loader/$FABRIC_LOADER.jar"
+CP="$CP:$GRADLE_CACHE/net.fabricmc/sponge-mixin/$SPONGE_MIXIN.jar"
+CP="$CP:$GRADLE_CACHE/com.google.code.gson/gson/$GSON.jar"
+CP="$CP:$GRADLE_CACHE/org.jspecify/jspecify/$JSPECIFY.jar"
+CP="$CP:$GRADLE_CACHE/com.mojang/brigadier/$BRIGADIER.jar"
+CP="$CP:$GRADLE_CACHE/it.unimi.dsi/fastutil/$FASTUTIL.jar"
 CP="$CP:libs/cloth-config-26.1.154.jar"
 CP="$CP:libs/modmenu-18.0.0.jar"
 
-JAVA_HOME="/home/cwright/.local/share/PandoraLauncher/runtime/graalvm-25.1.3/linux"
+JAVA_HOME="$HOME/.local/share/PandoraLauncher/runtime/graalvm-25.1.3/linux"
 $JAVA_HOME/bin/javac --release 25 -cp "$CP" -d build/classes $(find src/main/java -name "*.java")
 
 # Extract versions from gradle.properties
